6601cdb7572ba324d3bf7d8db13fa04f28e5043e
mebra005 <mebra005@fiu.edu>
2017-12-04 17:54:30 -0500

    Connected the admin reference Company section to backend - editable

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,4 +1,7 @@
+import { RefCompanyService } from './../shared/services/refCompany.service';
 import { Component, OnInit } from '@angular/core';
+import RefCompany from './../shared/models/refCompany.model';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _refCompanyService: RefCompanyService) { }
 
-  ngOnInit() {
+  public newRefCompany: RefCompany = new RefCompany();
+
+  refCompaniesList: RefCompany[];
+  editRefcompanies: RefCompany[] = [];
+
+  ngOnInit(): void {
+    this._refCompanyService.getRefCompanies()
+      .subscribe(refCompanies => {
+        this.refCompaniesList = refCompanies;
+        console.log(refCompanies);
+      });
   }
+
+  create() {
+    this._refCompanyService.createRefCompany(this.newRefCompany)
+      .subscribe((res) => {
+        this.refCompaniesList.push(res.data);
+        this.newRefCompany = new RefCompany();
+      });
+  }
+
+  editRefCompany(refCompany: RefCompany) {
+    console.log(refCompany);
+    if (this.refCompaniesList.includes(refCompany)) {
+      if (!this.editRefcompanies.includes(refCompany)) {
+        this.editRefcompanies.push(refCompany);
+      } else {
+        this.editRefcompanies.splice(this.editRefcompanies.indexOf(refCompany), 1);
+        this._refCompanyService.editRefCompany(refCompany).subscribe(res => {
+          console.log('Update Succesful');
+        }, err => {
+          this.editRefCompany(refCompany);
+          console.error('Update Unsuccesful');
+        });
+      }
+    }
+  }
+
+  submitRefCompany(event, refCompany: RefCompany) {
+    if (event.keyCode === 13) {
+      this.editRefCompany(refCompany);
+    }
+  }
+
+  deleteRefCompany(refCompany: RefCompany) {
+    this._refCompanyService.deleteRefCompany(refCompany._id).subscribe(res => {
+      this.refCompaniesList.splice(this.refCompaniesList.indexOf(refCompany), 1);
+    });
+  }
+
 
 }
