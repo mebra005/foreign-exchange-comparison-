f1c8b6655fbe72dfdcddac6ca4b20a39afc08543
mebra005 <mebra005@fiu.edu>
2017-12-06 15:38:46 -0500

    updated the app module with new services

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,12 +1,30 @@
+import { CompanyService } from './shared/services/company.service';
+import { RefCompanyService } from './shared/services/refCompany.service';
 import { TestBed, async } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        FormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        RefCompanyService,
+        CompanyService,
+        MediaDevices,
+        
       ],
     }).compileComponents();
   }));
@@ -15,18 +33,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RouterModule } from '@angular/router';
+import { MoniesService } from './shared/services/fees/monies.service';
 import { RefCompanyService } from './shared/services/refCompany.service';
 import { CompanyService } from './shared/services/company.service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -10,6 +12,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { MedianService } from './shared/services/median.service';
 
 @NgModule({
   declarations: [
@@ -22,10 +25,11 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
     NgbModule.forRoot()
   ],
-  providers: [RefCompanyService, CompanyService],
+  providers: [RefCompanyService, CompanyService, MedianService, MoniesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
