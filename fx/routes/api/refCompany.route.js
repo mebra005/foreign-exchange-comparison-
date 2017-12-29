a64676609ef3c7330331db5b0e30c511b473e0b2
mebra005 <mebra005@fiu.edu>
2017-12-29 11:04:37 -0500

    Updated the routes

--- fx/routes/api/refCompany.route.js
+++ fx/routes/api/refCompany.route.js
@@ -4,8 +4,6 @@ var router = express.Router()
 
 var RefCompanyController = require('../../controllers/refCompany.controller');
 
-
-
 router.get('/', RefCompanyController.getRefCompanies)
 router.post('/', RefCompanyController.createRefCompany)
 router.put('/', RefCompanyController.updateRefCompany)
